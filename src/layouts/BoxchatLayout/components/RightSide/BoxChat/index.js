f1733830e5e5de4f918cb00ec425110d0c6a678b
TranMinhHoang <hoang01697@gmail.com>
2022-11-30 14:13:29 +0700

    #

--- src/layouts/BoxchatLayout/components/RightSide/BoxChat/index.js
+++ src/layouts/BoxchatLayout/components/RightSide/BoxChat/index.js
@@ -13,10 +13,11 @@ function BoxChat() {
     const [state, setState] = useState({
         messages: [],
         name: '',
-        contentMessage: '',
+        typedMessage: '',
     });
     const messageEndRef = useRef();
     const clientRef = useRef();
+    const inputRef = useRef();
 
     useEffect(() => {
         scrollToBottom();
@@ -36,35 +37,39 @@ function BoxChat() {
             '/app/user-all',
             JSON.stringify({
                 name: state.name,
-                message: state.contentMessage,
+                message: state.typedMessage,
             }),
         );
+        setState((prev) => ({ ...prev, typedMessage: '' }));
+        inputRef.current.focus();
     };
 
     const handleChangeInput = (e) => {
         setState((prev) => ({
             ...prev,
-            contentMessage: e.target.value,
+            typedMessage: e.target.value,
         }));
     };
 
     const displayMessages = () => {
         return (
             <div>
-                {state.messages.map((msg) => {
+                {state.messages.map((msg, index) => {
                     return (
-                        <div>
+                        <div key={index}>
                             {state.name === msg.name ? (
-                                <div>
-                                    <p className="title1">{msg.name} : </p>
-                                    <br />
-                                    <p>{msg.message}</p>
+                                <div className={cx('message', 'my_message')}>
+                                    <div>
+                                        <p>{msg.message}</p>
+                                        <span>12:12</span>
+                                    </div>
                                 </div>
                             ) : (
-                                <div>
-                                    <p className="title2">{msg.name} : </p>
-                                    <br />
-                                    <p>{msg.message}</p>
+                                <div className={cx('message', 'frnd_message')}>
+                                    <div>
+                                        <p>{msg.message}</p>
+                                        <span>12:12</span>
+                                    </div>
                                 </div>
                             )}
                         </div>
@@ -79,8 +84,8 @@ function BoxChat() {
             <div className={cx('chatbox')}>
                 <NameComponent setName={setName} />
                 {displayMessages()}
-
                 <div ref={messageEndRef} />
+
                 <SockJsClient
                     url="http://localhost:8080/websocket-chat/"
                     topics={['/topic/user']}
@@ -99,11 +104,13 @@ function BoxChat() {
                     ref={clientRef}
                 />
             </div>
+
             <div className={cx('chatbox_input')}>
                 <FontAwesomeIcon className={cx('icon')} icon={faFaceSmile} />
                 <FontAwesomeIcon className={cx('icon')} icon={faImage} />
                 <input
-                    value={state.contentMessage}
+                    ref={inputRef}
+                    value={state.typedMessage}
                     type="text"
                     placeholder="Soạn tin nhắn"
                     onChange={handleChangeInput}
@@ -112,77 +119,6 @@ function BoxChat() {
                 <svg className={cx('send')} viewBox="0 0 24 30" onClick={sendMessage}>
                     <path d="M16.6915026,12.4744748 L3.50612381,13.2599618 C3.19218622,13.2599618 3.03521743,13.4170592 3.03521743,13.5741566 L1.15159189,20.0151496 C0.8376543,20.8006365 0.99,21.89 1.77946707,22.52 C2.41,22.99 3.50612381,23.1 4.13399899,22.8429026 L21.714504,14.0454487 C22.6563168,13.5741566 23.1272231,12.6315722 22.9702544,11.6889879 C22.8132856,11.0605983 22.3423792,10.4322088 21.714504,10.118014 L4.13399899,1.16346272 C3.34915502,0.9 2.40734225,1.00636533 1.77946707,1.4776575 C0.994623095,2.10604706 0.8376543,3.0486314 1.15159189,3.99121575 L3.03521743,10.4322088 C3.03521743,10.5893061 3.34915502,10.7464035 3.50612381,10.7464035 L16.6915026,11.5318905 C16.6915026,11.5318905 17.1624089,11.5318905 17.1624089,12.0031827 C17.1624089,12.4744748 16.6915026,12.4744748 16.6915026,12.4744748 Z"></path>
                 </svg>
-
-                {/* <div className={cx('message', 'my_message')}>
-                    <p>
-                        Hi
-                        <br />
-                        <span>12:12</span>
-                    </p>
-                </div>
-                <div className={cx('message', 'frnd_message')}>
-                    <p>
-                        hello
-                        <br />
-                        12:12
-                    </p>
-                </div>
-                <div className={cx('message', 'my_message')}>
-                    <p>
-                        Hi
-                        <br />
-                        <span>12:12</span>
-                    </p>
-                </div>
-                <div className={cx('message', 'frnd_message')}>
-                    <p>
-                        đang làm gì đấy
-                        <br />
-                        12:13
-                    </p>
-                </div>
-                <div className={cx('message', 'my_message')}>
-                    <p>
-                        Đang tìm điện thoại
-                        <br />
-                        <span>12:13</span>
-                    </p>
-                </div>
-                <div className={cx('message', 'frnd_message')}>
-                    <p>
-                        Tìm ở đâu
-                        <br />
-                        12:14
-                    </p>
-                </div>
-                <div className={cx('message', 'my_message')}>
-                    <p>
-                        Tìm chỗ mất chứ ở đâu
-                        <br />
-                        <span>12:15</span>
-                    </p>
-                </div>
-                <div className={cx('message', 'frnd_message')}>
-                    <p>
-                        Chán vậy
-                        <br />
-                        12:15
-                    </p>
-                </div>
-                <div className={cx('message', 'my_message')}>
-                    <p>
-                        ừ! buồn
-                        <br />
-                        <span>12:16</span>
-                    </p>
-                </div>
-                <div ref={messageEndRef} className={cx('message', 'frnd_message')}>
-                    <p>
-                        Thôi tìm đi! Hưng đi ngủ đây!chiều còn học
-                        <br />
-                        12:17
-                    </p>
-                </div> */}
             </div>
         </>
     );
